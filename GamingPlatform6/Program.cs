0fd8c4a94b4eda7f82461bbee3cca3f111500cfc
hakim <smyaj.abdelhakim@gmail.com>
2024-12-10 18:28:11 +0100

    Revert "db liée avec succés"
    This reverts commit 9955f6da3fdad7afe1ad7878c2356be8c86e77c4.

--- GamingPlatform6/Program.cs
+++ GamingPlatform6/Program.cs
@@ -13,7 +13,7 @@ namespace GamingPlatform6
             //=====Middleware and Services=============================================
             var builder = WebApplication.CreateBuilder(args);
 
-            // Configure Entity Framework avec la base de données SQLite ou SQL Serverg
+            // Configure Entity Framework avec la base de données SQLite ou SQL Server
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
